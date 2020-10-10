c35416f292afb2d87e97e32caeb3db0ee1cda61e
Timothy Coleman <timhc22@gmail.com>
2020-10-10 23:08:54 +0100

    Tidy up exports for import to be used

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { ErrorWithErrorHandlerError } = require('./lib/errors');
+const ErrorHandler  = require('./lib/ErrorHandler');
+const SlackErrorHandler  = require('./lib/SlackErrorHandler');
+const ErrorWithErrorHandlerError = require('./lib/errors');
 
 module.exports = {
   ErrorWithErrorHandlerError,
+  ErrorHandler,
+  SlackErrorHandler
 }
