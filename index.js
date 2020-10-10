f29b360c7e63c2d66400e8152aa663442c273a36
Timothy Coleman <timhc22@gmail.com>
2020-10-11 00:04:57 +0100

    Tweak exports files

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const ErrorHandler  = require('./lib/ErrorHandler');
-const SlackErrorHandler  = require('./lib/SlackErrorHandler');
-const ErrorWithErrorHandlerError = require('./lib/errors');
-
-module.exports = {
-  ErrorWithErrorHandlerError,
-  ErrorHandler,
-  SlackErrorHandler
-}
+module.exports = require('./lib');
