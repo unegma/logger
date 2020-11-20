403136aa28d8518a7df15f629b1ed8d74ce7bb51
Timothy Coleman <timhc22@gmail.com>
2020-11-20 15:17:50 +0000

    Add error logger classes

--- test/index.spec.js
+++ test/index.spec.js
@@ -10,6 +10,8 @@ chai.use(sinonChai);
 const nock = require('nock')
 const Logger = require('../lib/Logger');
 const SlackLogger = require('../lib/SlackLogger');
+const ErrorLogger = require('../lib/ErrorLogger');
+const SlackErrorLogger = require('../lib/SlackErrorLogger');
 const ErrorWithLoggerError = require('../lib/errors/ErrorWithLoggerError');
 
 describe('Errors Test', () => {
@@ -44,7 +46,7 @@ describe('Errors Test', () => {
 
   it('should handle an Error', () => {
     const logger = new Logger();
-    logger.log('testError', 'This is a test');
+    logger.log('testLog', 'This is a test');
     expect(console.log).to.have.been.calledTwice;
   });
 
@@ -54,7 +56,46 @@ describe('Errors Test', () => {
       .reply(200, "ok");
 
     const slackLogger = new SlackLogger(SLACK_LOG_URL); // 'YYYY-MM-DD-dddd'
-    const response = await slackLogger.log('testError', 'This is a test');
+    const response = await slackLogger.log('testLog', 'This is a test');
+    expect(console.log).to.have.callCount(3);
+    expect(response).to.equal('ok');
+  });
+
+
+  it('should create an instance of an ErrorWithLoggerError', () => {
+    const error = new ErrorWithLoggerError('Error Happened');
+    expect(error.message).to.equal('Error Happened');
+  });
+
+  it('should create an instance of an ErrorLogger', () => {
+    const errorLogger = new ErrorLogger();
+    expect(errorLogger).to.be.instanceOf(ErrorLogger);
+    expect(() => {
+      errorLogger.throwError('Message')
+    }).to.throw(ErrorWithLoggerError);
+  });
+
+  it('should create an instance of a SlackErrorLogger', () => {
+    const slackErrorLogger = new SlackErrorLogger(SLACK_LOG_URL);
+    expect(slackErrorLogger).to.be.instanceOf(SlackErrorLogger);
+    expect(() => {
+      slackErrorLogger.throwError()
+    }).to.throw(ErrorWithLoggerError);
+  });
+
+  it('should handle an Error', () => {
+    const errorLogger = new ErrorLogger();
+    errorLogger.logError('testError', 'This is a test', 'Test Error');
+    expect(console.log).to.have.been.calledTwice;
+  });
+
+  it('should handle an Error and Post to Slack', async () => {
+    nock(SLACK_LOG_URL)
+        .post(uri => uri.includes('services'))
+        .reply(200, "ok");
+
+    const slackErrorLogger = new SlackErrorLogger(SLACK_LOG_URL); // 'YYYY-MM-DD-dddd'
+    const response = await slackErrorLogger.logError('testError', 'This is a test', 'Test Error');
     expect(console.log).to.have.callCount(3);
     expect(response).to.equal('ok');
   });
